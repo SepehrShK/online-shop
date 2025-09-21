2cfdaf4881078e3b0b23dec350966d1a7e2583b0
Sepehr Shayan Kia <sepehrshayankia@gmail.com>
2025-09-21 18:24:16 +0330

    nav complete change

--- src/navbar/Navbar.tsx
+++ src/navbar/Navbar.tsx
@@ -4,49 +4,54 @@ import { Link } from "react-router-dom";
 import './Navbar.css'
 import { useAuth } from "../context/useAuth";
 import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
     const { logout, isLoggedIn } = useAuth();
-    const [open, setOpen] = useState(false);
+    const [openProfile, setOpenProfile] = useState<boolean>(false);
+    const [menuOpen, setMenuOpen] = useState<boolean>(false)
 
     return (
         <nav className="nav">
             <div className="right-nav">
-                <ul className="nav-links">
-                    <li className="home-link">
-                        <Link to="/">home</Link>
+                {/* نحوه نمایش لینک ها به اندازه صفحه و فشرده شدن دکمه منو بستگی داره */}
+                <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
+                    <li className="nav-link">
+                        <Link to="/">خانه</Link>
                     </li>
+                    {/* اگه فرد وارد نشده باشه */}
+                    {!isLoggedIn && (
+                        <li className="nav-link" >
+                            <Link to="/Login">ورود</Link>
+                        </li>
+                    )}
+                    {/* اگه فرد وارد شده باشه */}
+                    {isLoggedIn && (
+                        <li className="nav-link" onClick={() => setOpenProfile(!openProfile)}>
+                            <RiArrowDropDownLine className={`arrow-icon ${openProfile ? "up" : ""}`} size={30}/>
+                            <span>ادمین</span>
+                            
+                            {/* باز یا بسته بودن پروفایل */}
+                            {openProfile && (
+                                <ul className="dropdown-menu">
+                                    <li className="logout-button" onClick={() => {
+                                        logout()
+                                        setOpenProfile(false)
+                                    }}>خروج از حساب</li>
+                                </ul>
+                            )}
+                        </li>
+                    )}
                 </ul>
+                {/* دکمه منو هنگام کوچیک بودن صفحه */}
+                <div className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+                    <GiHamburgerMenu className="hamburger-icon" size={30} />
+                </div>
             </div>
+            {/* فیلد سرچ محصول */}
             <div className="input-div">
                 {<CiSearch size={20}/>}
                 <input className="input-field" placeholder="جست و جو..." />
-            </div>
-            <div className="left-nav">
-                {/* اگه فرد وارد نشده باشه */}
-                {!isLoggedIn && (
-                    <button className="loginButton-userDropdown" type="button">
-                        <span><Link to="/Login">ورود</Link></span>
-                    </button>
-                )}
-                {/* اگه فرد وارد شده باشه */}
-                {isLoggedIn && (
-                    <div className="dropdown-box">
-                        <button type="button" className="loginButton-userDropdown" onClick={() => setOpen(!open)}>
-                            <RiArrowDropDownLine className={`arrow-icon ${open ? "up" : ""}`} size={30}/>
-                            <span>ادمین</span>
-                        </button>
-
-                        {open && (
-                            <ul className="dropdown-menu">
-                                <li className="logout-button" onClick={() => {
-                                    logout()
-                                    setOpen(false)
-                                }}>خروج از حساب</li>
-                            </ul>
-                        )}
-                    </div>
-                )}
             </div>
         </nav>
     )
